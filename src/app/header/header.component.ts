a37bd887f70165d492ca8d8f5b9959ac594c41cc
Stefan Grigorescu <grigorescu2001@gmail.com>
2023-03-04 04:16:15 +0200

    Added: Navigation bar style Changed: Moved unused styles out of styles.scss, into a separate unused scss file (in order to improve reload time) Changed: Moved toggle style out of styles.scss, into tournament-list.component.scss (since it was used only there; in order to improve reload time)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
-import { RoutesManager } from '../../common/routes-manager';
 
 @Component({
     selector: 'app-header',
-    templateUrl: './header.component.html'
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
     collapsed = true;
-
-    routesManager = RoutesManager;
-
 }
