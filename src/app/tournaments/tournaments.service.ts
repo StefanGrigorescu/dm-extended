28f31723a7dcea661db9a981fb47c62bfe30d26f
Stefan Grigorescu <grigorescu2001@gmail.com>
2023-03-05 02:29:11 +0200

    Improved: Card Pool Component responsiveness Improved: Tournament Detail Component responsiveness and layout Improved: Tournament Component responsiveness Added: A new mock tournament in order to chesk how the Tournament Component looks like with tournaments with long texts

--- src/app/tournaments/tournaments.service.ts
+++ src/app/tournaments/tournaments.service.ts
@@ -47,10 +47,20 @@ export class TournamentsService {
             (function(d){ d.setDate(d.getDate()-1); return d})(new Date),
             TournamentState.Ended
         ),
+        5: new Tournament(
+            "From its medieval origins to the digital era, learn everything there is to know about the ubiquitous lorem ipsum passage.",
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            "HISTORY, PURPOSE AND USAGE\nLorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:\nL“Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nThe purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn't distract from the layout. A practice not without controversy, laying out pages with meaningless filler text can be very useful when the focus is meant to be on design, not content.\nThe passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum.",
+            (function(d){ d.setDate(d.getDate()-28); return d})(new Date),
+            (function(d){ d.setDate(d.getDate()-23); return d})(new Date),
+            (function(d){ d.setDate(d.getDate()-11); return d})(new Date),
+            (function(d){ d.setDate(d.getDate()-1); return d})(new Date),
+            TournamentState.Archived
+        ),
 
         // The archived tournaments will be filtered and depending on the current view being redered,
         // either them or the current tournaments will be displayed in the list
-        5: new Tournament(
+        6: new Tournament(
             "Mock Archived State Tournament 1",
             "archived (short desc. here)",
             "Some descriptive text",
@@ -60,7 +70,7 @@ export class TournamentsService {
             (function(d){ d.setDate(d.getDate()-1); return d})(new Date),
             TournamentState.Archived
         ),
-        6: new Tournament(
+        7: new Tournament(
             "Mock Archived State Tournament 2",
             "short desc.: archived",
             "Some descriptive text",
