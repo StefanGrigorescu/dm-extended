bfdd14b72e31675bda50d3dbe7535749c464b9f6
Stefan Grigorescu <grigorescu2001@gmail.com>
2023-03-06 05:02:43 +0200

    Added: Logic to retrieve the available width of a component and adjuts the children css classes accordingly Refactored: Moved screen sizes variables out of custom-container into separate scss file Added: ScreenSizes ts class to match the siezes variables in the scss file

--- src/app/cards/card-pool/card-pool.component.ts
+++ src/app/cards/card-pool/card-pool.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, HostListener, Input, ViewChild } from '@angular/core';
+import { ScreenSizes } from 'src/common/screen-sizes';
 import { Cards } from '../cards-model';
 
 @Component({
@@ -6,8 +7,38 @@ import { Cards } from '../cards-model';
   templateUrl: './card-pool.component.html',
   styleUrls: ['./card-pool.component.scss']
 })
-export class CardPoolComponent {
+export class CardPoolComponent implements AfterViewInit {
   @Input() cards: Cards;
+
+  availableWidth: number;
+  @ViewChild('poolContainer') poolContainer: ElementRef;
+  ScreenSizes = ScreenSizes;
+
+  ngAfterViewInit(): void {
+    this.availableWidth = this.poolContainer.nativeElement.offsetWidth;
+    
+    console.log("availableWidth is in init: " + this.availableWidth);
+  }
+
+  ngAfterViewChecked(): void {
+    this.availableWidth = this.poolContainer.nativeElement.offsetWidth;
+    console.log("availableWidth is in check: " + this.availableWidth);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.availableWidth = this.poolContainer.nativeElement.offsetWidth;
+  }
+
+  isAvailableWidthBetween(first: number, second: number): boolean {
+    return this.availableWidth >= first && this.availableWidth <= second;
+  }
+  isAvailableWidthGreaterThan(other: number) {
+    return this.availableWidth >= other;
+  }
+  isAvailableWidthLowerThan(other: number) {
+    return this.availableWidth <= other;
+  }
 
   setSortBy(sortBy: string): void {
     var oldSortBy = this.cards.sortBy;
---
+++ src/common/screen-sizes.ts
@@ -0,0 +1,9 @@
+// The numeric values representic screen sizes (in pixels)
+// Should match the values in the screen-sizes.scss file
+export class ScreenSizes {
+    static smMin: number = 768;
+    static mdMin: number = 992;
+    static lgMin: number = 1200;
+
+    static xsMax: number = ScreenSizes.smMin - 1;
+}
